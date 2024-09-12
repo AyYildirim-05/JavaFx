deef4d4dda27dda18762c384d6f3912bf31c37e4
Ahmet Yusuf Yildirim <150823826+AhMYuf@users.noreply.github.com>
2024-09-11 23:21:54 -0400

    Commit 4: In this version, we learn how to switch scenes and how to setup multiple scenes in a simple way.

--- src/main/java/edu/vanier/template/MainApp.java
+++ src/main/java/edu/vanier/template/MainApp.java
@@ -1,39 +1,47 @@
 package edu.vanier.template;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+    Stage window;
+    Scene scene1, scene2;
     Button btn;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        btn = new Button();
-        btn.setText("Say 'Hello World'");
+    public void start(Stage primaryStage) throws Exception{
+        window = primaryStage;
 
-        /*
-        Utilizing lambda expressions to simplify the process
-        If we add {} after the arrow, we can do multiple actions
-         */
-        btn.setOnAction(event -> {
-            System.out.println("Lambda expression working...");
-            System.out.println("This is good...");
-        });
+        // Button 1
+        Label label = new Label("Welcome to scene 1!");
+        Button button1 = new Button("Go to scene 2?");
+        button1.setOnAction(e -> window.setScene(scene2)); // Code to switch between two scenes
 
+        // layout 1
+        VBox layout1 = new VBox(20); // Will space the elements by 20 pixels.
+        layout1.getChildren().addAll(label, button1); // Use .add() if it is a single element
+        scene1 = new Scene(layout1, 300, 300); // Create a scene based on the layout we designed
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        // Button 2
+        Button button2 = new Button("This scene sucks go back to scene 1?");
+        button2.setOnAction(e -> window.setScene(scene1));
+
+        // layout 2
+        StackPane layout2 = new StackPane();
+        layout2.getChildren().addAll(button2);
+        scene2 = new Scene(layout2, 600, 600); // This line is the one that creates the scene with our layout
+
+        window.setScene(scene1); // this code makes so that we have to start with scene one
+        window.setTitle("Small Attempt...");
+        window.show();
     }
 }
